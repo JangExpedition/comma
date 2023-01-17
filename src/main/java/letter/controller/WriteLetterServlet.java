2cd6822c7acc874fa33186bcf02cc8769a85075d
Jangcccci <jangcccci@gmail.com>
2023-01-18 00:04:14 +0900

    장원정 - header.jsp 수정

--- src/main/java/letter/controller/WriteLetterServlet.java
+++ src/main/java/letter/controller/WriteLetterServlet.java
@@ -26,8 +26,6 @@ public class WriteLetterServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
