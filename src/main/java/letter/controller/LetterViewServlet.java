84d7ca440d4ad01c342d7fdb8309d52d5c965da8
gangs-99 <alsk6021@naver.com>
2023-01-26 15:19:11 +0900

    counselingViewer.css 수정

--- src/main/java/letter/controller/LetterViewServlet.java
+++ src/main/java/letter/controller/LetterViewServlet.java
@@ -3,12 +3,10 @@ package letter.controller;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import common.Attachment;
 import common.CommaUtils;
 import letter.model.dto.Letter;
 import letter.model.service.LetterService;
@@ -27,32 +25,6 @@ public class LetterViewServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 사용자 입력값
 		int letterNo = Integer.parseInt(request.getParameter("no"));
-
-		
-		String letterCookieVal = "";
-		boolean hasRead = false;
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				String name = cookie.getName();
-				String value = cookie.getValue();
-				
-				if ("letter".equals(name)) {
-					letterCookieVal = value;
-					if (value.contains("[" + letterNo + "]")) {
-						hasRead = true;
-					} // if () end
-				} // if () end
-			} // for each end
-		} // if () end
-		
-		// 응답쿠키
-		if (!hasRead) {
-			Cookie cookie = new Cookie("letter", letterCookieVal + "[" + letterNo + "]");
-			cookie.setMaxAge(365 * 24 * 60 * 60); // 365일
-			cookie.setPath(request.getContextPath() + "/letter/letterView");
-			response.addCookie(cookie);
-		} // if() end
 		
 		// 업무로직
 		Letter letter = letterService.selectOneLetter(letterNo);
